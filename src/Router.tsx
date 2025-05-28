91a98e1941d3c94b59e37d6a6d15975dbf01c594
skcheongbrian <skcheongbrian@gmail.com>
2025-05-28 16:05:31 +0800

    Change router to hash router
    Change router to hash router
    
    This is done so that users can copy and paste the url

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { ContactPage } from '@/pages/Contact.page';
 import { Ay2425S1Page } from '@/pages/Ay2425-s1.page';
 import { Ay2425S2Page } from '@/pages/Ay2425-s2.page';
 import { Ay2526S1Page } from '@/pages/Ay2526-s1.page';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: '/',
@@ -28,9 +28,6 @@ const router = createBrowserRouter(
       element: <Ay2526S1Page />
     },
   ],
-  {
-    basename: '/~bskch',
-  }
 );
 
 export function Router() {
